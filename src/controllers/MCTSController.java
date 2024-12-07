846fcfd8570bc2c05e71378bba56cf4b3e50b5cb
Nikolexion <nfalc2003@gmail.com>
2024-12-07 12:58:35 -0300

    feat: avance en metodos de MCTS

--- src/controllers/MCTSController.java
+++ src/controllers/MCTSController.java
@@ -2,19 +2,23 @@ package controllers;
 
 import dungeon.play.GameCharacter;
 import dungeon.play.PlayMap;
+import dungeon.play.Hero;
 
-public class MCTSController implements Controller {
-    private PlayMap playMap;
-    private GameCharacter hero;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class MCTSController extends Controller {
+    private Random random;
 
     public MCTSController(PlayMap playMap, GameCharacter hero) {
-        this.playMap = playMap;
-        this.hero = hero;
+        super(playMap, hero);
+        this.random = new Random();
     }
 
     @Override
     public int getNextAction() {
-        // Implementar el algoritmo Monte Carlo Tree Search aquí
+        System.out.println("MCTSController.getNextAction()");
         return mcts();
     }
 
@@ -24,8 +28,98 @@ public class MCTSController implements Controller {
     }
 
     private int mcts() {
-        // Implementar la lógica de MCTS aquí
-        // Retornar la acción seleccionada (0, 1, 2, 3)
-        return 0; // Placeholder
+        System.out.println("MCTS: Starting MCTS");
+        Node root = new Node(null, -1, map.clone());
+        for (int i = 0; i < 1000; i++) { // Número de iteraciones
+            /* System.out.println("MCTS: Iteration " + i); */
+            Node node = select(root);
+            if (!node.playMap.isGameHalted()) {
+                expand(node);
+                Node child = node.children.get(random.nextInt(node.children.size()));
+                double reward = simulate(child);
+                backpropagate(child, reward);
+            }
+        }
+        int bestAction = bestAction(root);
+        /* System.out.println("MCTS: Best action selected: " + bestAction); */
+        return bestAction;
+    }
+
+    private Node select(Node node) {
+       /*  System.out.println("MCTS: Selecting node"); */
+        while (!node.children.isEmpty()) {
+            node = node.children.stream().max((n1, n2) -> Double.compare(ucb1(n1), ucb1(n2))).get();
+        }
+        return node;
+    }
+
+    private void expand(Node node) {
+        /* System.out.println("MCTS: Expanding node"); */
+        for (int action = 0; action < 4; action++) { // Asumiendo 4 acciones posibles
+            PlayMap newMap = node.playMap.clone();
+            newMap.updateGame(action);
+            node.children.add(new Node(node, action, newMap));
+        }
+    }
+
+    private double simulate(Node node) {
+        /* System.out.println("MCTS: Simulating node"); */
+        PlayMap simulationMap = node.playMap.clone();
+        while (!simulationMap.isGameHalted()) {
+            int action = random.nextInt(4); // Asumiendo 4 acciones posibles
+            simulationMap.updateGame(action);
+        }
+        double reward = calculateReward(simulationMap);
+        /* System.out.println("MCTS: Simulation reward: " + reward); */
+        return reward;
+    }
+
+    private double calculateReward(PlayMap simulationMap) {
+        Hero hero = simulationMap.getHero();
+        int score = hero.getScore();
+        int hitpoints = hero.getHitpoints();
+        int treasuresCollected = simulationMap.getRewardChars().size() - simulationMap.getDeadRewardArray().length;
+        int monstersKilled = simulationMap.getMonsterChars().size() - simulationMap.getDeadMonsterArray().length;
+        int potionsUsed = simulationMap.getPotionChars().size() - simulationMap.getDeadPotionArray().length;
+    
+        double reward = 100 + score + hitpoints + treasuresCollected * 10 + monstersKilled * 5 + potionsUsed * 2;
+        /* System.out.println("MCTS: Calculated reward: " + reward); */
+        return reward;
+    }
+
+    private void backpropagate(Node node, double reward) {
+        /* System.out.println("MCTS: Backpropagating reward"); */
+        while (node != null) {
+            node.visits++;
+            node.value += reward;
+            node = node.parent;
+        }
+    }
+
+    private double ucb1(Node node) {
+        return node.value / node.visits + Math.sqrt(2 * Math.log(node.parent.visits) / node.visits);
+    }
+
+    private int bestAction(Node root) {
+        /* System.out.println("MCTS: Selecting best action"); */
+        return root.children.stream().max((n1, n2) -> Double.compare(n1.visits, n2.visits)).get().action;
+    }
+
+    private class Node {
+        Node parent;
+        List<Node> children;
+        int action;
+        PlayMap playMap;
+        int visits;
+        double value;
+
+        Node(Node parent, int action, PlayMap playMap) {
+            this.parent = parent;
+            this.action = action;
+            this.playMap = playMap;
+            this.children = new ArrayList<>();
+            this.visits = 0;
+            this.value = 0;
+        }
     }
 }
--- src/experiment/DebugMode.java
+++ src/experiment/DebugMode.java
@@ -1,54 +1,45 @@
 package experiment;
 
-import java.util.Scanner;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.BufferedWriter;
-import java.io.IOException;
-
-import controllers.*;
-
+import controllers.MCTSController;
 import dungeon.Dungeon;
 import dungeon.DungeonLoader;
 import dungeon.play.PlayMap;
-import dungeon.visualization.PlayVisualizer;
 
-import util.math2d.Matrix2D;
-import util.statics.StatisticUtils;
+import java.util.Scanner;
+import java.io.File;
 
 public class DebugMode {
-	final int maxActions = 300;
-	
-	public void runTest(String filename){
-		String[] temp = filename.split("/");
-		String mapFile = temp[temp.length-1];
-		
-		String asciiMap = "";
-		try { 
-			asciiMap = new Scanner(new File(filename)).useDelimiter("\\A").next(); 
-		} catch(Exception e){
-			System.out.println(e.toString());
-		}
-		Dungeon testDungeon = DungeonLoader.loadAsciiDungeon(asciiMap);
-		PlayMap testPlay = new PlayMap(testDungeon);
-		testPlay.startGame();
+    final int maxActions = 300;
 
-		Controller testAgent = new RoombaController(testPlay,testPlay.getHero());
+    public void runTest(String filename) {
+        String[] temp = filename.split("/");
+        String mapFile = temp[temp.length-1];
 
-		int actions = 0;
+        String asciiMap = "";
+        try {
+            asciiMap = new Scanner(new File(filename)).useDelimiter("\\A").next();
+        } catch(Exception e){
+            System.out.println(e.toString());
+        }
+        Dungeon testDungeon = DungeonLoader.loadAsciiDungeon(asciiMap);
+        PlayMap testPlay = new PlayMap(testDungeon);
+        testPlay.startGame();
 
-		System.out.println(testPlay.toASCII(true));
-		while(!testPlay.isGameHalted() && actions<maxActions){
-			testPlay.updateGame(testAgent.getNextAction());
-			actions++;
-			System.out.println("----- ACTION "+actions+" -----");
-			System.out.println(testPlay.toASCII(true));
-			//System.out.println(PlayVisualizer.renderHeatmapDungeon(testPlay));
-		}
-	}
-	
-	public static void main(String[] args) {
-		DebugMode exp = new DebugMode();
-		exp.runTest("./dungeons/map0.txt");
-	}
+        MCTSController testAgent = new MCTSController(testPlay, testPlay.getHero());
+
+        int actions = 0;
+
+        System.out.println(testPlay.toASCII(true));
+        while(!testPlay.isGameHalted() && actions < maxActions){
+            testPlay.updateGame(testAgent.getNextAction());
+            actions++;
+            System.out.println("----- ACTION " + actions + " -----");
+            System.out.println(testPlay.toASCII(true));
+        }
+    }
+
+    public static void main(String[] args) {
+        DebugMode exp = new DebugMode();
+        exp.runTest("./dungeons/map0.txt");
+    }
 }
